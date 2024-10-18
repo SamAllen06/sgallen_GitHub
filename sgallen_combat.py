1a91d21015cce7221a4507209f242ec81793029f
SamAllen06 <samgallen06@gmail.com>
2024-10-18 19:38:13 -0400

    combat project
    Added combat project and tbcModule that I created

---
+++ sgallen_combat.py
@@ -0,0 +1,53 @@
+import tbcModule
+def main():
+    c1 = tbcModule.Character()
+    c1.name = input("What would you like to name your character? ")
+    c1.health = tbcModule.testInt(c1.name, "health", "hitPoints", 0, 100)
+    c1.maxDamage = tbcModule.testInt(c1.name, "strength", "maxDamage", 0, 100)
+    c1.hitChance = tbcModule.testInt(c1.name, "success rate", "hitChance", 0, 100)
+    c1.armor = tbcModule.testInt(c1.name, "armor absorption", "armor", 0, 100)
+    c2 = tbcModule.Character()
+    c2.name = "The Monster"
+    c2.health = tbcModule.testInt("the monster", "health", "hitpoints", 0, 100)
+    c2.maxDamage = tbcModule.testInt("the monster", "strength", "maxDamage", 0, 100)
+    c2.hitChance = tbcModule.testInt("the monster", "success rate", "hitChance", 0, 100)
+    c2.armor = tbcModule.testInt("the monster", "armor absorption", "armor", 0, 100)
+    print(f"{c1.name}'s Stats")
+    print("====================")
+    print(f"Health: {c1.health:14}")
+    print(f"Strength: {c1.maxDamage:12}")
+    print(f"Success Rate: {c1.hitChance:8}")
+    print(f"Armor Absorption: {c1.armor:4}")
+    print()
+    print(f"The Monster's Stats")
+    print("====================")
+    print(f"Health: {c2.health:14}")
+    print(f"Strength: {c2.maxDamage:12}")
+    print(f"Success Rate: {c2.hitChance:8}")
+    print(f"Armor Absorption: {c2.armor:4}")
+    print()
+    
+    keepGoing = True
+    while keepGoing:
+        nextRound = input("Proceed in the fight? (Y/N) ")
+        print()
+        if nextRound.upper() == "N":
+            keepGoing = False
+        if nextRound.upper() == "Y":
+            c2.health = tbcModule.doFight(c1, c2)
+            c1.health = tbcModule.doFight(c2, c1)
+        else:
+            print("Sorry, that's not a valid response. Please try again")
+        if c1.health <= 0:
+            if c2.health <= 0:
+                print("It's a tie! No one wins.")
+                keepGoing = False
+            else:
+                print(f"Sorry {c1.name}, you lose.")
+                keepGoing = False
+        elif c2.health <= 0:
+            print(f"Congratulations {c1.name}! You win!")
+            keepGoing = False
+
+if __name__ == "__main__":
+    main()
---
+++ tbcModule.py
@@ -0,0 +1,60 @@
+import random
+class Character():
+    def __init__(self):
+        super().__init__()
+        self.name = "unnamed"
+        self.hitPoints = 0
+        self.hitChance = 0
+        self.maxDamage = 0
+        self.armor = 0
+    
+    def propertyMaker(self, value, trait):
+        @property
+        def trait(self):
+            return self.__trait
+        
+        @trait.setter
+        def trait(self, value):
+            self.__trait = value
+
+def testInt(character, traitName, trait, minimum, maximum):
+    keepGoing = True
+    while keepGoing:
+        traitResponse = input(f"What would you like {character}'s {traitName} to be? ")
+        try:
+            traitResponse = int(traitResponse)
+            if traitResponse <= int(maximum):
+                if traitResponse >= int(minimum):
+                    trait = traitResponse
+                    keepGoing = False
+                else:
+                    print(f"Sorry, that's smaller than the excepted minimum: {minimum}. Please try again. ")
+            else:
+                print(f"Sorry, that's larger than the excepted maximum: {maximum}. Please try again. ")
+        except:
+            print(f"Sorry, {traitResponse} is not an integer. Please try again. ")
+    return trait
+
+def printStats(character, traitName, trait):
+    print(f"{character}'s {traitName} is {trait}.")
+
+def statValueGetter(playerStat):
+    randomStat = random.randint(1, playerStat)
+    return randomStat
+    
+
+def doFight(personOne, personTwo):
+    damageDealt = statValueGetter(personOne.maxDamage)
+    print(f"{personOne.name} attacked {personTwo.name} for {damageDealt} points!")
+    successAnswer = statValueGetter(100)
+    if successAnswer > personOne.hitChance:
+        damageDealt = 0
+        print(f"But {personOne.name}'s attack missed.")
+    else:
+        armorBlocked = statValueGetter(personTwo.armor)
+        print(f"{personTwo.name}'s armor blocked {armorBlocked} points!")
+        if armorBlocked <= damageDealt:
+            totalDamage = damageDealt - armorBlocked
+            personTwo.health = personTwo.health - totalDamage
+        print(f"{personTwo.name} now has {personTwo.health} health left!")
+    return personTwo.health
