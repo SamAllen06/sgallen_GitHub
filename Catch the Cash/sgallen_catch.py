2b2f2d01a3182445af0b5186e1906681a617faf2
SamAllen06 <samgallen06@gmail.com>
2024-10-30 10:35:22 -0400

    4th milestone for catch the cash game
    added mushroom movement with reset and boundary behaviors

--- Catch the Cash/sgallen_catch.py
+++ Catch the Cash/sgallen_catch.py
@@ -2,6 +2,29 @@ import pygame, simpleGE, random
 
 """Catch the cash game demo"""
 
+class Mushroom(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("feltMushroom.png")
+        self.setSize(44, 56)
+        self.minSpeed = 3
+        self.maxSpeed = 8
+        self.reset()
+        
+    def reset(self):
+        #move to top of the screen
+        self.y = -100
+        
+        #x is random from 22 to screen width minus 22
+        self.x = random.randint(22, self.screenWidth-22)
+        
+        #dy is a random minSpeed to maxSpeed
+        self.dy = random.randint(self.minSpeed, self.maxSpeed)
+        
+    def checkBounds(self):
+        if self.top > self.screenHeight:
+            self.reset()
+
 class Ronald(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -21,8 +44,11 @@ class Game(simpleGE.Scene):
         super().__init__()
         self.setImage("mountainBackground.jpg")
         self.ronald = Ronald(self)
+        self.mushroom = Mushroom(self)
         
-        self.sprites = [self.ronald]
+        self.sprites = [self.ronald, self.mushroom]
+        
+    
         
 def main():
     game = Game()
